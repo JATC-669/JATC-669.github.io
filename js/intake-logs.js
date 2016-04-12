dff9b165bf4b4f894779df4efe84034d34102bc7
Christopher Collins <chris@spring215.com>
2016-04-12 17:46:00 -0400

    finished lesson schedule

--- js/intake-logs.js
+++ js/intake-logs.js
@@ -1,86 +1,16 @@
-//Generates Current Date and adds it to the form
-function getDate() {
+function load() {
 
-	var dateGen = new Date();
-	var date = (dateGen.getMonth() + 1) + '/' + dateGen.getDate() + '/' +  dateGen.getFullYear();
+	// Get the current date
+	var date = new Date()
+	var month = date.getMonth() + 1;
+	var day = date.getDate();
+	var year = date.getFullYear();
 
-	document.getElementById('dateField').value = date;
+	// Sets the value of the backDate field to the current date
+	var backDateField = document.getElementById('backDate')
+	backDateField.value = month + "/" + day + "/" + year;
 }
 
-// Adds Entered info to the list below
-function addLog () {
-
-// Identifies the input fields
-var log = {
-	date: 			document.getElementById("dateField"),
-	studentId: 		document.getElementById("studentId"),
-	lastName: 		document.getElementById("lastName"),
-	firstName: 		document.getElementById("firstName"),
-	course: 		document.getElementById("course"),
-	lessons: 		document.getElementById("lessons"),
-	finalIndicator: document.getElementById("finalIndicator")
-	};
-
-	// Creates an array with the field values
-	var arr = [log.date.value,
-			   '@' + log.studentId.value, 
-			   log.lastName.value,
-		  	   log.firstName.value,
-		       log.course.value,
-		       log.lessons.value,
-		       log.finalIndicator.value];
-
-	// Counts the number of <tr>'s in the report log
-	var numberOfTrs = document.querySelectorAll('#intakeLog tr').length;
-
-
-		// Creates a TR
-		var tableRow                      = document.createElement('tr');
-
-		// Creates cells for TR
-		var tableCellloggedDate           = document.createElement('td');
-		var tableCellloggedStudentId      = document.createElement('td');
-		var tableCellloggedLastName 	  = document.createElement('td');
-		var tableCellloggedFirstName 	  = document.createElement('td');
-		var tableCellloggedCourse 		  = document.createElement('td');
-		var tableCellloggedLessons 		  = document.createElement('td');
-		var tableCellloggedfinalIndicator = document.createElement('td');
-
-		// Add IDs to cells in TR
-		tableCellloggedDate.id 		  = "loggedDate" + numberOfTrs;
-		tableCellloggedStudentId.id   = "loggedStudentId" + numberOfTrs;
-		tableCellloggedLastName.id 	  = "loggedLastName" + numberOfTrs;
-		tableCellloggedFirstName.id   = "loggedFirstName" + numberOfTrs;
-		tableCellloggedCourse.id   	  = "loggedCourse" + numberOfTrs;
-		tableCellloggedLessons.id   	  = "loggedLessons" + numberOfTrs;
-		tableCellloggedfinalIndicator.id   	  = "loggedfinalIndicator" + numberOfTrs;
-
-		tableRow.id = "row" + numberOfTrs++;
-		var report = document.getElementById('intakeLog');
-		
-		// Appends Table Row to the report
-		report.appendChild(tableRow);
-
-		// Appends the cells to the Table ROw
-		tableRow.appendChild(tableCellloggedDate);
-		tableRow.appendChild(tableCellloggedStudentId);
-		tableRow.appendChild(tableCellloggedLastName);
-		tableRow.appendChild(tableCellloggedFirstName);
-		tableRow.appendChild(tableCellloggedCourse);
-		tableRow.appendChild(tableCellloggedLessons);
-		tableRow.appendChild(tableCellloggedfinalIndicator);
-		
-
-	
-
-	// alert(numberOfTrs)
-
-	document.getElementById("loggedDate" + (numberOfTrs - 1)).innerHTML 	      = arr[0];
-	document.getElementById("loggedStudentId" + (numberOfTrs - 1)).innerHTML 	  = arr[1];
-	document.getElementById("loggedLastName" + (numberOfTrs - 1)).innerHTML  	  = arr[2];
-	document.getElementById("loggedFirstName" + (numberOfTrs - 1)).innerHTML 	  = arr[3];
-	document.getElementById("loggedCourse" + (numberOfTrs - 1)).innerHTML 		  = arr[4];
-	document.getElementById("loggedLessons" + (numberOfTrs - 1)).innerHTML 		  = arr[5];
-	document.getElementById("loggedfinalIndicator" + (numberOfTrs - 1)).innerHTML = arr[6];
-
+function populateTable() {
+	alert('hello')
 }
--- js/lesson-schedule.js
+++ js/lesson-schedule.js
@@ -1,5 +1,8 @@
-function valad() {
+/*global document: false */
 
+// Allows submission if all fields are filled out
+function valad() {
+    'use strict';
     // Sets vars for the inputs in the fields
     var courseNumber = document.getElementById('course').value;
     var submissionType = document.getElementById('type').value;
@@ -16,105 +19,207 @@ function valad() {
     var nextDay = document.getElementById('nextDay');
     var onlineSubmission = document.getElementById('onlineSubmission');
 
-    // When any field is changed, check to unchanged fields. I think this is where the issue is.
+    // When any field is changed, check to see if the shipping method should change. If there are empty fields don't change the submit button
     if (courseNumber === 'dis' || submissionType === 'dis' || shipMonth === 'dis' || shipDay === 'dis' || shipYear === 'dis' || shipMethod === 'dis')
     {     
-
-
-        if ((courseNumber === '04' || courseNumber === '07' || courseNumber === '19')) {
-            
+        // Changes shipping method options if its a handgraded course
+        if ((courseNumber === '04' || courseNumber === '07' || courseNumber === '19')) {      
             initShipMethod.disabled = true;
-            onlineSubmission.disabled = true;
-            
+            onlineSubmission.disabled = true;          
             twoDay.disabled = false;
             nextDay.disabled = false;
             regGround.disabled = false;
-
-            regGround.selected = true;
             onlineSubmission.selected = false;
-
         }
 
-        else if ((submissionType === 'f') && (courseNumber != '04' || courseNumber != '07' || courseNumber != '19')) {
-            
+        // Changes shipping method options if its a final from an online course
+        else if ((submissionType === 'f') && (courseNumber != '04' || courseNumber != '07' || courseNumber != '19')) {       
             initShipMethod.disabled = true;
             onlineSubmission.disabled = true;
-            
             twoDay.disabled = false;
             nextDay.disabled = false;
             regGround.disabled = false;
-
-            regGround.selected = true;
             onlineSubmission.selected = false;
         }
 
+        // Changes shipping method options if its a online submission
         else  {
-
-            initShipMethod.disabled = true;
-            
+            initShipMethod.disabled = true;    
             twoDay.disabled = true;
             nextDay.disabled = true;
             regGround.disabled = true;
-
             onlineSubmission.selected = true;
-
         }
     }
 
+    // When any field is changed, check to see if the shipping method should change. Change the submit button
     else
     {
-        if ((courseNumber === '04' || courseNumber === '07' || courseNumber === '19')) {
-            
+        if ((courseNumber === '04' || courseNumber === '07' || courseNumber === '19')) {    
             initShipMethod.disabled = true;
-            onlineSubmission.disabled = true;
-            
+            onlineSubmission.disabled = true;  
             twoDay.disabled = false;
             nextDay.disabled = false;
             regGround.disabled = false;
-
-            regGround.selected = true;
             onlineSubmission.selected = false;
-
             document.getElementById('submitButton').className = 'expand';
             document.getElementById('submitButton').innerHTML = 'SUBMIT';
 
         }
 
         else if ((submissionType === 'f') && (courseNumber != '04' || courseNumber != '07' || courseNumber != '19')) {
-            
             initShipMethod.disabled = true;
             onlineSubmission.disabled = true;
-            
             twoDay.disabled = false;
             nextDay.disabled = false;
             regGround.disabled = false;
-
-            regGround.selected = true;
             onlineSubmission.selected = false;
-
             document.getElementById('submitButton').className = 'expand';
             document.getElementById('submitButton').innerHTML = 'SUBMIT';
         }
 
-        else  {
-
+        else if ((submissionType === 'l') && (courseNumber != '04' || courseNumber != '07' || courseNumber != '19')) {
             initShipMethod.disabled = true;
-            
             twoDay.disabled = true;
             nextDay.disabled = true;
             regGround.disabled = true;
-
+            onlineSubmission.disabled = false;
             onlineSubmission.selected = true;
-
             document.getElementById('submitButton').className = 'expand';
             document.getElementById('submitButton').innerHTML = 'SUBMIT';
-
         }
-    };
+    }
+}
 
-};
+function courseScript() {
+    var courseNumber = document.getElementById('course').value;
+    var submissionType = document.getElementById('type');
+    var shipMonth = document.getElementById('month');
+    var shipDay = document.getElementById('day');
+    var shipYear = document.getElementById('year');
+    var shipMethod = document.getElementById('method').value;
+    var shipMethodNumber = Number(shipMethod);
+    var output = document.getElementById('recTimeOutput');
+    var shipDate = new Date(shipYear.value, shipMonth.value, shipDay.value);
+    var weekday;
+    var month;
+    var shippingDays = parseInt(shipMethod);
+
+    var processDate = new Date(shipYear.value, shipMonth.value, shipDay.value);
+
+
+    var gradedDate = new Date();
+
+    var gradingDays = 5;
+  
+    function dateStringCreator(day) {
+        var weekday = new Array(7);
+            weekday[0]=  "Sunday";
+            weekday[1] = "Monday";
+            weekday[2] = "Tuesday";
+            weekday[3] = "Wednesday";
+            weekday[4] = "Thursday";
+            weekday[5] = "Friday";
+            weekday[6] = "Saturday";
+            return weekday[day];
+    }
+    
+    function monthStringCreator(month) {
+        var theMonth = new Array(12);
+            theMonth[0]=  "Jan";
+            theMonth[01]=  "Feb";
+            theMonth[02]=  "Mar";
+            theMonth[03]=  "Apr";
+            theMonth[04]=  "May";
+            theMonth[05]=  "Jun";
+            theMonth[06]=  "Jul";
+            theMonth[07]=  "Aug";
+            theMonth[08]=  "Sept";
+            theMonth[09]=  "Oct";
+            theMonth[10]=  "Nov";
+            theMonth[11]=  "Dec";
+            return theMonth[month];
+    }
+
+    // If a submission, lesson or final, is from SPF 4, 7 or 19
+    if (courseNumber === '04' || courseNumber === '07' || courseNumber === '19') {
+      
+        processDate.setDate(shipDate.getDate()+shippingDays+gradingDays);
+
+        // If the upload date falls on a Sat then add two days
+        if (processDate.getDay() === 6) {
+            processDate.setDate(processDate.getDate()+2);
+
+            weekday = dateStringCreator(processDate.getDay());
+            month = monthStringCreator(processDate.getMonth());
+            output.innerHTML = 'Your final will be graded around:<strong> ' + weekday + ',&nbsp;' + month + '&nbsp;' + processDate.getDate() + ', ' + processDate.getFullYear() + '</strong>. You can check your status on the <a href="https://jatc669.wccnet.edu/login/index.php">Testing Website</a>.';
+            document.getElementById('recTimeOutput').className = 'recTimeOutputShown fade';
+        }
+
+        // If the upload date falls on a Sun then add one day
+        else if (processDate.getDay() === 0) {
+            processDate.setDate(processDate.getDate()+1);
+
+            weekday = dateStringCreator(processDate.getDay());
+            month = monthStringCreator(processDate.getMonth());
+            output.innerHTML = 'Your final will be graded around:<strong> ' + weekday + ',&nbsp;' + month + '&nbsp;' + processDate.getDate() + ', ' + processDate.getFullYear() + '</strong>. You can check your status on the <a href="https://jatc669.wccnet.edu/login/index.php">Testing Website</a>.';
+            document.getElementById('recTimeOutput').className = 'recTimeOutputShown fade';
+        }
+
+        // If the upload date falls on a weekday, add no days
+        else {
+            weekday = dateStringCreator(processDate.getDay());
+            month = monthStringCreator(processDate.getMonth());
+            output.innerHTML = 'Your final will be graded around:<strong> ' + weekday + ',&nbsp;' + month + '&nbsp;' + processDate.getDate() + ', ' + processDate.getFullYear() + '</strong>. You can check your status on the <a href="https://jatc669.wccnet.edu/login/index.php">Testing Website</a>.';
+            document.getElementById('recTimeOutput').className = 'recTimeOutputShown fade';
+        }
+    }
+
+    // If the submission is an online lesson
+    else if (submissionType.value === 'l') {
+        shipDate = shipDate.setDate(shipDate.getDate());
+        output.innerHTML = 'Your online submission should be uploaded and recorded. You can check your status on the <a href="https://jatc669.wccnet.edu/login/index.php">Testing Website</a>.';
+        document.getElementById('recTimeOutput').className = 'recTimeOutputShown fade';
+    }
+
+    // If the submission is a final from an online course
+    else if (submissionType.value === 'f') {
+        processDate.setDate(shipDate.getDate()+shippingDays);
+
+        if (processDate.getDay() === 6) {
+            processDate.setDate(processDate.getDate()+2);
+            weekday = dateStringCreator(processDate.getDay());
+            month = monthStringCreator(processDate.getMonth());
+            output.innerHTML = 'Your final will be graded around:<strong> ' + weekday + ',&nbsp;' + month + '&nbsp;' + processDate.getDate() + ', ' + processDate.getFullYear() + '</strong>. You can check your status on the <a href="https://jatc669.wccnet.edu/login/index.php">Testing Website</a>.';
+            document.getElementById('recTimeOutput').className = 'recTimeOutputShown fade';  
+        }
+
+        // If the upload date falls on a Sun then add one day
+        else if (processDate.getDay() === 0) {
+            processDate.setDate(processDate.getDate()+1);
+            weekday = dateStringCreator(processDate.getDay());
+            month = monthStringCreator(processDate.getMonth());
+            output.innerHTML = 'Your final will be graded around:<strong> ' + weekday + ',&nbsp;' + month + '&nbsp;' + processDate.getDate() + ', ' + processDate.getFullYear() + '</strong>. You can check your status on the <a href="https://jatc669.wccnet.edu/login/index.php">Testing Website</a>.';
+            document.getElementById('recTimeOutput').className = 'recTimeOutputShown fade';
+        }
+
+        // If the upload date falls on a weekday, add no days
+        else {
+            weekday = dateStringCreator(processDate.getDay());
+            month = monthStringCreator(processDate.getMonth());
+            output.innerHTML = 'Your final will be graded around:<strong> ' + weekday + ',&nbsp;' + month + '&nbsp;' + processDate.getDate() + ', ' + processDate.getFullYear() + '</strong>. You can check your status on the <a href="https://jatc669.wccnet.edu/login/index.php">Testing Website</a>.';
+            document.getElementById('recTimeOutput').className = 'recTimeOutputShown fade';
+            document.getElementById('submitButton').className = 'expand';
+        }
+    }
+
+    else {
+
+    }
+}
 
 function badClick() {
+
     var courseNumber = document.getElementById('course').value;
     var submissionType = document.getElementById('type').value;
     var shipMonth = document.getElementById('month').value;
@@ -130,130 +235,4 @@ function badClick() {
     else {
         courseScript();
     }
-}
-
-function courseScript() {
-    var courseNumber = document.getElementById('course').value;
-    var submissionType = document.getElementById('type');
-    var shipMonth = document.getElementById('month');
-    var shipDay = document.getElementById('day');
-    var shipYear = document.getElementById('year');
-    var shipMethod = document.getElementById('method').value;
-
-    var shipMethodNumber = Number(shipMethod);
-
-    var output = document.getElementById('recTimeOutput');
-    
-    var shipDate = new Date(shipYear.value, shipMonth.value, shipDay.value)
-
-    function dateStringCreator(day) {
-        var weekday = new Array(7);
-            weekday[0]=  "Sunday";
-            weekday[1] = "Monday";
-            weekday[2] = "Tuesday";
-            weekday[3] = "Wednesday";
-            weekday[4] = "Thursday";
-            weekday[5] = "Friday";
-            weekday[6] = "Saturday";
-
-            return weekday[day];
-    }
-
-    function monthStringCreator(month) {
-        var theMonth = new Array(12);
-            theMonth[00]=  "Jan";
-            theMonth[01]=  "Feb";
-            theMonth[02]=  "Mar";
-            theMonth[03]=  "Apr";
-            theMonth[04]=  "May";
-            theMonth[05]=  "Jun";
-            theMonth[06]=  "Jul";
-            theMonth[07]=  "Aug";
-            theMonth[08]=  "Sept";
-            theMonth[09]=  "Oct";
-            theMonth[10]=  "Nov";
-            theMonth[11]=  "Dec";
-
-            return theMonth[month];
-    }
-
-    // If a submission, lesson or final, is from SPF 4, 7 or 19
-    if (courseNumber === '04' || courseNumber === '07' || courseNumber === '19') {
-        
-        // Takes the ship date entered, adds the number of shipping days, adds 5 grading days
-        shipDate === shipDate.setDate(shipDate.getDate()+shipMethodNumber+5);
-
-        // If the upload date falls on a Sat then add two days
-        if (shipDate.getDay() === 6) {
-            shipDate === shipDate.setDate(shipDate.getDate()+2);
-            var weekday = dateStringCreator(shipDate.getDay());
-            var month = monthStringCreator(shipDate.getMonth());
-            
-            output.innerHTML = 'Your submission will be graded around: <strong> ' + weekday + ',&nbsp;' + month + '&nbsp;' + shipDate.getDate() + ', ' + shipDate.getFullYear() + '</strong>. You can check your status on the <a href="https://jatc669.wccnet.edu/login/index.php">Testing Website</a>.';
-            document.getElementById('recTimeOutput').className = 'recTimeOutput fade';
-        }
-
-        // If the upload date falls on a Sun then add one day
-        else if (shipDate.getDay() === 0) {
-            shipDate === shipDate.setDate(shipDate.getDate()+1);
-            var weekday = dateStringCreator(shipDate.getDay());
-            var month = monthStringCreator(shipDate.getMonth());
-
-            output.innerHTML = 'Your submission will be graded around:<strong> ' + weekday + ',&nbsp;' + month + '&nbsp;' + shipDate.getDate() + ', ' + shipDate.getFullYear() + '</strong>. You can check your status on the <a href="https://jatc669.wccnet.edu/login/index.php">Testing Website</a>.';
-            document.getElementById('recTimeOutput').className = 'recTimeOutput fade';
-        }
-
-        // If the upload date falls on a weekday, add no days
-        else {
-            var weekday = dateStringCreator(shipDate.getDay());
-            var month = monthStringCreator(shipDate.getMonth());
-
-            output.innerHTML = 'Your submission will be graded around:<strong> ' + weekday + ',&nbsp;' + month + '&nbsp;' + shipDate.getDate() + ', ' + shipDate.getFullYear() + '</strong>. You can check your status on the <a href="https://jatc669.wccnet.edu/login/index.php">Testing Website</a>.';
-            document.getElementById('recTimeOutput').className = 'recTimeOutput fade';
-        }
-    }
-
-    // If the submission is an online lesson
-    else if (submissionType.value === 'l') {
-        shipDate === shipDate.setDate(shipDate.getDate());
-
-        output.innerHTML = 'Your online submission should be uploaded and recorded. You can check your status on the <a href="https://jatc669.wccnet.edu/login/index.php">Testing Website</a>.';
-        document.getElementById('recTimeOutput').className = 'recTimeOutput fade'
-
-    }
-
-    // If the submission is a final from an online course
-    else if (submissionType.value === 'f') {
-        shipDate === shipDate.setDate(shipDate.getDate()+shipMethodNumber+1);
-
-        // If the upload date falls on a Sat then add two days
-        if (shipDate.getDay() === 6) {
-            shipDate === shipDate.setDate(shipDate.getDate()+2);
-            var weekday = dateStringCreator(shipDate.getDay());
-            var month = monthStringCreator(shipDate.getMonth());
-
-            output.innerHTML = 'Your final will be graded around:<strong> ' + weekday + ',&nbsp;' + month + '&nbsp;' + shipDate.getDate() + ', ' + shipDate.getFullYear() + '</strong>. You can check your status on the <a href="https://jatc669.wccnet.edu/login/index.php">Testing Website</a>.';
-        }
-
-        // If the upload date falls on a Sun then add one day
-        else if (shipDate.getDay() === 0) {
-            shipDate === shipDate.setDate(shipDate.getDate()+1);
-            var weekday = dateStringCreator(shipDate.getDay());
-            var month = monthStringCreator(shipDate.getMonth());
-
-            output.innerHTML = 'Your final will be graded around:<strong> ' + weekday + ',&nbsp;' + month + '&nbsp;' + shipDate.getDate() + ', ' + shipDate.getFullYear() + '</strong>. You can check your status on the <a href="https://jatc669.wccnet.edu/login/index.php">Testing Website</a>.';
-            document.getElementById('recTimeOutput').className = 'recTimeOutput fade';
-        }
-
-        // If the upload date falls on a weekday, add no days
-        else {
-            var weekday = dateStringCreator(shipDate.getDay());
-            var month = monthStringCreator(shipDate.getMonth());
-
-            output.innerHTML = 'Your final will be graded around:<strong> ' + weekday + ',&nbsp;' + month + '&nbsp;' + shipDate.getDate() + ', ' + shipDate.getFullYear() + '</strong>. You can check your status on the <a href="https://jatc669.wccnet.edu/login/index.php">Testing Website</a>.';
-            document.getElementById('recTimeOutput').className = 'recTimeOutput fade';
-        }
-    }
-
-
 }
